af37db50c5b6ab1ecb2db3ae13386b2c1aa31a15
Advait Dhakad <59308251+AdvaitDhakad@users.noreply.github.com>
2024-03-28 20:27:09 +0530

    inital upload

---
+++ src/app/emp-add-edit/emp-add-edit.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-emp-add-edit',
+  templateUrl: './emp-add-edit.component.html',
+  styleUrl: './emp-add-edit.component.scss',
+})
+export class EmpAddEditComponent {
+  empForm: FormGroup;
+  education: string[] = ['Matric', 'Diploma', 'Graduate', 'Post Graduate'];
+
+  constructor(private _fb: FormBuilder) {
+    this.empForm = this._fb.group({
+      firstName: '',
+      lastName: '',
+      email: '',
+      dob: '',
+      gender: '',
+      education: '',
+      company: '',
+      exp: '',
+      package: '',
+    });
+  }
+  onFormSubmit() {
+    if (this.empForm.valid) {
+      console.log(this.empForm.value);
+    }
+  }
+}
